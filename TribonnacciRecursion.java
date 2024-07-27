cb361aa4f8fdcea01dd2eab1c93ce9f8b6b0d0e0
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-07-27 19:21:20 +0300

    Tribonacci

---
+++ TribonnacciRecursion.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class TribonnacciRecursion {
+    public static void main(String[] args) {
+        System.out.println(trib(8));
+    }
+
+    public static int trib(int n) {
+        return trib(n, new HashMap<>());
+    }
+
+    public static int trib(int n, HashMap<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        int result = trib(n-1, memo) + trib(n-2, memo) + trib(n-3, memo);
+        return result;
+    }
+}

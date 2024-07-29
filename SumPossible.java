8671133d3a1f48b41409d659df0b09b308d827c4
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-07-29 16:35:11 +0300

    improvedTimeComplex

--- SumPossible.java
+++ SumPossible.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 
 public class SumPossible {
@@ -10,16 +11,26 @@ public class SumPossible {
     }
 
     public static boolean sumPossible(int amount, List<Integer> numbers) {
+        return sumPossible(amount, numbers, new HashMap<>());
+    }
+
+    public static boolean sumPossible(int amount, List<Integer> numbers, HashMap<Integer, Boolean> memo) { 
         if(amount == 0) return true;
 
         if(amount < 0) return false;
 
+        if (memo.containsKey(amount)) {
+            return memo.get(amount);
+        }
+
         for(int number : numbers) {
             int subAmount = amount - number;
             if (sumPossible(subAmount, numbers)) {
+                memo.put(number, true);
                 return true;
             }
         }
+        memo.put(amount, false);
         return false;
     }
 }

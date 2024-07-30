942b373a9c330444de3f2455aa098fd13778f46b
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-07-30 18:01:12 +0300

    minChange

---
+++ MinChange.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.HashMap;
+
+public class MinChange{
+	public static void main(String[] args) {
+//		List<Integer> coins = List.of(1, 2, 3);
+		List<Integer> coins = List.of(3,5,6,1,10);
+		int amount = 12;
+		int result = minChange(amount, coins);
+			System.out.println(result);
+		}
+	
+	public static int minChange(int amount, List<Integer> coins) {
+		return minChange(amount, coins, new HashMap<>());
+	}
+
+	public static int minChange(int amount, List<Integer> coins, HashMap<Integer, Integer> memo) {
+		if(amount == 0) {
+			return 0;
+		}
+
+		if(amount < 0) {
+			return -1;
+		}	
+		
+		if(memo.containsKey(amount)) {
+			return memo.get(amount);
+		}
+
+		int minCoins = -1;
+		for(int coin : coins) {
+			int subAmount = amount - coin;
+			int subCoins =  minChange(subAmount, coins, memo);
+		
+			if(subCoins != -1) {	
+			int numCoins = subCoins + 1;
+
+				if(numCoins < minCoins || minCoins == -1) {
+					minCoins = numCoins;
+			}	
+		}	
+	}
+
+	memo.put(amount, minCoins);
+	return minCoins;
+}
+
+
+}

8f4f348a2ab7d455afa7b5653130775e2ff0f8ec
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-15 19:24:11 +0300

    customUser+Pass

--- SpringSecExample/src/main/java/com/hastycode/SpringSecExample/HomeController.java
+++ SpringSecExample/src/main/java/com/hastycode/SpringSecExample/HomeController.java
@@ -1,5 +1,6 @@
 package com.hastycode.SpringSecExample;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "Welcome home!";
+    public String home(HttpServletRequest request) {
+        return "Welcome home!" + request.getSession().getId();
     }
 }

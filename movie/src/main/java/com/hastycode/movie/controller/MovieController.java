30aabe9e17e239d409e22a09c1ccdc2c7084777c
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-16 09:41:40 +0300

    updated

--- movie/src/main/java/com/hastycode/movie/controller/MovieController.java
+++ movie/src/main/java/com/hastycode/movie/controller/MovieController.java
@@ -3,21 +3,23 @@ package com.hastycode.movie.controller;
 import com.hastycode.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/movies")
 public class MovieController {
 
-    private MovieService service;
+    private final MovieService service;
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String home() {
         return "Welcome to movies";
     }
 
-    @GetMapping("/movies")
+    @GetMapping()
     public String getMovieDetails(@RequestParam String name) {
         return service.getMovieDetails(name);
     }
--- movie/src/main/java/com/hastycode/movie/service/MovieService.java
+++ movie/src/main/java/com/hastycode/movie/service/MovieService.java
@@ -1,11 +1,17 @@
 package com.hastycode.movie.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class MovieService {
 
     @Autowired
@@ -15,6 +21,7 @@ public class MovieService {
     private String apiKey;
 
     @Value("${movie.api.url}")
+
     private String apiUrl;
 
     @Value("${movie.api.host}")
@@ -22,7 +29,17 @@ public class MovieService {
 
     public String getMovieDetails(String name) {
         String url = apiUrl + "?s=" + name + "&r=json&page=1";
+        var headers = new HttpHeaders();
 
-        return restTemplate.getForObject(url, String.class);
+
+
+        headers.set("x-rapidapi-key", apiKey);
+        headers.set("x-rapidapi-host", apiHost);
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+
+        log.info("these are the headers sent {}", requestEntity);
+
+        var response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        return response.getBody();
     }
 }

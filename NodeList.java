56e8483fadbae41ff788cebc99c3fb3a95d5cf41
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-23 07:35:25 +0300

    randomShi

---
+++ NodeList.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NodeList {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(8, 9, 4, 5, 3);
+        Collections.reverse(numbers);
+        System.out.println(numbers);
+    }
+    
+}

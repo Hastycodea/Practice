f21562e4974306785666f0fac16204796855cda5
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-23 07:57:22 +0300

    RandomRange

--- NodeList.java
+++ NodeList.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +9,9 @@ public class NodeList {
         List<Integer> numbers = Arrays.asList(8, 9, 4, 5, 3);
         Collections.reverse(numbers);
         System.out.println(numbers);
+
+        ArrayList<Integer> words = new ArrayList<>();
+        
     }
     
 }
---
+++ RandomNumber.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumber {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        // int number = rand.nextInt(100); // range from 0-99 inclusive
+        // int number2 = rand.nextInt(100) + 1; // range from 1-100 inclusive
+
+        // How many random numbers should be printed?
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many numbers random numbers should be printed? ");
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < input; i++) {
+            int randomNumber = rand.nextInt(11);
+            System.out.println(randomNumber);
+        }
+
+    }
+    
+}

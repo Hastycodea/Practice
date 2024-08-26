7381677bfe051226b9a82750feffa29d2a0c6114
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-26 22:09:24 +0300

    stringContains

---
+++ StringOperation.java
@@ -0,0 +1,10 @@
+public class StringOperation {
+    public static void main(String[] args) {
+        String name = "andrehPM";
+        if (name.contains("PM")) {
+            System.out.println("Yes it contains");
+        } else {
+            System.out.println("Noma");
+        }
+    }
+}

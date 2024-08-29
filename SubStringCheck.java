35fea8f9bb5b1a1c22c2f0599840b8d10f41fb30
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-29 11:11:01 +0300

    substring

---
+++ SubStringCheck.java
@@ -0,0 +1,8 @@
+public class SubStringCheck {
+    public static void main(String[] args) {
+        String name = "Andreh";
+
+        String result = name.substring(0, name.length()-2);
+        System.out.println(result);
+    }
+}

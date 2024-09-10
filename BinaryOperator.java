6e74fb2d54ae50c4446586b14b01e98d29b3c925
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-10 06:38:57 +0300

    binary Operations

---
+++ BinaryOperator.java
@@ -0,0 +1,17 @@
+public class BinaryOperator {
+    public static void main(String[] args) {
+        int number = 3;
+        System.out.println(~2);
+
+
+        // Binary String
+        String binaryString = Integer.toBinaryString(number);
+        //
+        String result = String.format("%32s", binaryString).replace(' ', '0');
+        System.out.println(result);
+        
+        double value = 0.9393939393;
+        System.out.println(String.format("%.2f", value));
+
+    }
+}

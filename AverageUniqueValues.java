9c0b36a46c6db6e1df82f43d60666c5f2c3d6d58
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-17 18:53:59 +0300

    summaryStats

--- AverageUniqueValues.java
+++ AverageUniqueValues.java
@@ -1,10 +1,16 @@
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 public class AverageUniqueValues {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(5, 10, -3, 10, 20, 5, -1);
+        IntSummaryStatistics result = numbers.stream()
+                                .mapToInt(n -> Integer.valueOf(n))
+                                .summaryStatistics();
+        System.out.println(result);
+        System.out.println(result.getAverage());
         System.out.println(averageUnique(numbers));
         
     }

b402879b79d63640156632307838b46b051520cd
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-12 03:44:31 +0300

    HashMap Vim Tutorial

--- HashMapTutorial.java
+++ HashMapTutorial.java
@@ -12,16 +12,47 @@ public class HashMapTutorial {
         map.put(6, "six");
         map.put(7, "seven");
 
+        HashMap<Integer, String> numbersMap = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+        map.put(6, "six");
+        map.put(7, "seven");
+
+        System.out.println("Printing all the keys");
+
+        for (int key : map.keySet()) {
+            System.out.println(key);
+        }
+
+        System.out.println("Printing all the values");
+
+        for (String value : map.values()) {
+            System.out.println(value);
+        }
+
+
+        System.out.println("Printing the keyset");
+
+        for(Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry);
+        }
+
         // System.out.println(map.get(3));
 
         // System.out.println(map.getOrDefault(4, "not present"));
-        
+
         // testing entryset
-        for(Map.Entry<Integer, String> entry : map.entrySet()) {
-            // System.out.println(entry);
-            if(entry.getValue().equals("two")) System.out.println(entry.getKey() + " was found!");
-            if(entry.getValue().equals("four")) System.out.println(entry.getKey() + " was found!");
-            if(entry.getValue().equals("five")) System.out.println(entry.getKey() + " was found!");
-        }
+        // for(Map.Entry<Integer, String> entry : map.entrySet()) {
+        // // System.out.println(entry);
+        // if(entry.getValue().equals("two")) System.out.println(entry.getKey() + " was
+        // found!");
+        // if(entry.getValue().equals("four")) System.out.println(entry.getKey() + " was
+        // found!");
+        // if(entry.getValue().equals("five")) System.out.println(entry.getKey() + " was
+        // found!");
+        // }
     }
 }

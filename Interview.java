e4a34075be3a9851fa20cc4f69f6921f547dffd8
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-11-20 10:06:37 +0300

    intPrep

--- Interview.java
+++ Interview.java
@@ -1,12 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Interview {
     public static void main(String[] args) {
         // System.out.println(reverseString("Andreh"));
         // String name = "Andreh Brian Andere - 45 / #";
         // System.out.println(name.replaceAll("[^a-zA-Z0-9]", "").toLowerCase());
-		String name = "A man, a plan, a canal, Panama!";
-        String ship = "titanic";
-        System.out.println(palindrome(name));
-        System.out.println(palindrome(ship));
+		// String name = "A man, a plan, a canal, Panama!";
+        // String ship = "titanic";
+        // System.out.println(palindrome(name));
+        // System.out.println(palindrome(ship));
+
+        Map<String, Integer> students = new HashMap<>();
+
+        students.put("Andreh", 45);
+        students.put("Kim", 43);
+
+        // print all
+        System.out.println(students.toString());
+
+        // get value
+        System.out.println(students.get("Andreh"));
+
+        // confirm key
+        System.out.println(students.containsKey("Kim"));
+
+        // iterating all values and key
+        for ( Map.Entry<String, Integer> map : students.entrySet() ) {
+            System.out.println(map.getKey() + ":" + map.getValue());
+        }
 
     }
 

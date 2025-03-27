4c61cdf706e2982d797332d14c57d7e9739b7137
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-03-27 17:13:58 +0300

    SampleCoded

---
+++ Sample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String[] args) {
+        String s = "Hackerrank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        String s2 = scan.nextLine();
+
+        System.out.println(s + s2);
+    }
+    
+}

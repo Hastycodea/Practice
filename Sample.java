574371fd8628e2f8de3fc177d4de084a2dcfae0e
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-03-27 17:45:08 +0300

    nextLine

--- Sample.java
+++ Sample.java
@@ -6,9 +6,15 @@ public class Sample {
 
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Enter your age: ");
+        int age = scan.nextInt();
+
+        scan.nextLine();
+
+        System.out.print("Enter your name: ");
         String s2 = scan.nextLine();
 
-        System.out.println(s + s2);
+        System.out.println(s2 + " is of age " + age);
     }
     
 }
